913510dbb3b95399a6fc6775b69a3365b149555d
Adrian Mohnacs <amohnacss@gmail.com>
2019-05-07 23:31:29 -0700

    replaced missing files

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,15 +1,15 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity ^0.4.25;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
-
   modifier restricted() {
     if (msg.sender == owner) _;
+  }
+
+  constructor() public {
+    owner = msg.sender;
   }
 
   function setCompleted(uint completed) public restricted {
