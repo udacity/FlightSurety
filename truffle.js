3db4a3c3f58847a2156d7fc9d611045dee54eee5
Bobby <kestenfariz@gmail.com>
2021-12-19 16:16:14 +0100

    Save used packages + ganache settings

--- truffle.js
+++ truffle.js
@@ -1,14 +1,14 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "evidence cricket apple able utility illegal magic decorate law time hire sport";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      // gas: 9999999
     }
   },
   compilers: {
