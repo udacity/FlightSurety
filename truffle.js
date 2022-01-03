86b26639c21de871a6693e04126919f8f031fdcd
Bobby <kestenfariz@gmail.com>
2022-01-03 22:14:53 +0100

    Renew first configurations

--- truffle.js
+++ truffle.js
@@ -1,11 +1,11 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "evidence cricket apple able utility illegal magic decorate law time hire sport";
+var mnemonic = "grace orphan drink execute picnic ordinary festival rare net below rose attitude";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
       network_id: '*',
       // gas: 9999999
