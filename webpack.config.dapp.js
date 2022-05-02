74572de4ca767cded87eca928d5dcd063167586f
Tiger Wang <tigerwang@outlook.com>
2022-05-02 17:46:01 -0400

    wip

--- webpack.config.dapp.js
+++ webpack.config.dapp.js
@@ -40,8 +40,14 @@ module.exports = {
     extensions: [".js"]
   },
   devServer: {
-    contentBase: path.join(__dirname, "dapp"),
     port: 8000,
-    stats: "minimal"
+    devMiddleware: {
+      stats: "minimal"
+    },
+    static: {
+      staticOptions: {
+        contentBase: path.join(__dirname, "dapp")
+      }
+    }
   }
 };
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -4,35 +4,37 @@ const nodeExternals = require('webpack-node-externals')
 const StartServerPlugin = require('start-server-webpack-plugin')
 
 module.exports = {
-    entry: [
-        'webpack/hot/poll?1000',
-        './src/server/index'
-    ],
-    watch: true,
-    target: 'node',
-    externals: [nodeExternals({
-        whitelist: ['webpack/hot/poll?1000']
-    })],
-    module: {
-        rules: [{
-            test: /\.js?$/,
-            use: 'babel-loader',
-            exclude: /node_modules/
-        }]
-    },
-    plugins: [
-        new StartServerPlugin('server.js'),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.DefinePlugin({
-            "process.env": {
-                "BUILD_TARGET": JSON.stringify('server')
-            }
-        }),
-    ],
-    output: {
-        path: path.join(__dirname, 'prod/server'),
-        filename: 'server.js'
-    }
+  entry: [
+    'webpack/hot/poll?1000',
+    './src/server/index'
+  ],
+  watch: true,
+  target: 'node',
+  externals: [nodeExternals({
+    allowlist: ['webpack/hot/poll?1000']
+  })],
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new StartServerPlugin('server.js'),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "BUILD_TARGET": JSON.stringify('server')
+      }
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, 'prod/server'),
+    filename: 'server.js'
+  },
+  optimization: {
+    moduleIds: 'named'
+  }
 }
