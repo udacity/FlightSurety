e5df0c729e124f28b78d37d4d9a62e132d0425dc
Tiger Wang <tigerwang@outlook.com>
2022-05-02 16:05:56 -0400

    wip

--- test/flightSurety.js
+++ test/flightSurety.js
@@ -7,7 +7,7 @@ contract('Flight Surety Tests', async (accounts) => {
   var config;
   before('setup contract', async () => {
     config = await Test.Config(accounts);
-    await config.flightSuretyData.authorizeCaller(config.flightSuretyApp.address);
+    //await config.flightSuretyData.authorizeCaller(config.flightSuretyApp.address);
   });
 
   /****************************************************************************************/
