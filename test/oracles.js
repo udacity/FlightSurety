84039ce82f89fac1816483b15b9c759799156216
Tiger Wang <tigerwang@outlook.com>
2022-05-02 16:38:27 -0400

    wip

--- test/oracles.js
+++ test/oracles.js
@@ -2,21 +2,20 @@
 var Test = require('../config/testConfig.js');
 //var BigNumber = require("@ethersproject/bignumber");
 
+// Watch contract events
+const STATUS_CODE_UNKNOWN = 0;
+const STATUS_CODE_ON_TIME = 10;
+const STATUS_CODE_LATE_AIRLINE = 20;
+const STATUS_CODE_LATE_WEATHER = 30;
+const STATUS_CODE_LATE_TECHNICAL = 40;
+const STATUS_CODE_LATE_OTHER = 50;
+
 contract('Oracles', async (accounts) => {
 
   const TEST_ORACLES_COUNT = 9;
   var config;
   before('setup contract', async () => {
     config = await Test.Config(accounts);
-
-    // Watch contract events
-    const STATUS_CODE_UNKNOWN = 0;
-    const STATUS_CODE_ON_TIME = 10;
-    const STATUS_CODE_LATE_AIRLINE = 20;
-    const STATUS_CODE_LATE_WEATHER = 30;
-    const STATUS_CODE_LATE_TECHNICAL = 40;
-    const STATUS_CODE_LATE_OTHER = 50;
-
   });
 
 
